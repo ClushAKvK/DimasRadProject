50286b4e2cf477abf4005c3c55d19732af2d935c
ClushA <shirko.02@mail.ru>
2023-05-21 02:41:57 +0300

    init Dimas project

---
+++ RadProject/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+using Microsoft.Office.Interop.Excel;
+
+namespace RadProject
+{
+    public partial class Form1 : Form
+    {
+
+        // англ. названия колонок для программы
+        string[] report_columns = new string[] { "title", "amount", "summary_price" };
+
+        // рус. названия колонок для екселя
+        string[] ru_report_columns = new string[] { "Название товара", "Количество", "Сумма" };
+
+        // инфа которую будем выгружать в ексель, по факту лист массивов строк
+        List<string[]> data = new List<string[]>();
+
+        // словарь для связки id объекта CheckListBox и id-клиента из таб. Client
+        Dictionary<int, int> client_ids = new Dictionary<int, int>();
+
+        DataSet ds = new DataSet();
+        System.Data.DataTable dt = new System.Data.DataTable();
+        NpgsqlConnection con;
+
+        public Form1(NpgsqlConnection con)
+        {
+            InitializeComponent();
+
+            this.con = con;
+            InitClients();
+        }
+
+        // Функция для инициализации checkListBox: ФИО клиентов
+        public void InitClients() {
+            string sql = "SELECT * FROM Client;";
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, this.con);
+            ds.Reset();
+            da.Fill(ds);
+            dt = ds.Tables[0];
+
+            // отдельно ставим на 0 позицию параметр Все, для быстрого выбора ВСЕХ клиентов
+            checkedListBox1.Items.Add("Все");
+            checkedListBox1.ItemCheck += ItemCheck;
+            int clb_client = 1;
+            foreach (DataRow row in dt.Rows) {
+                var cells = row.ItemArray;
+                checkedListBox1.Items.Add(cells[1] + " " + cells[2]);
+
+                // Связка записи в checkedListBox1 и id-клиента
+                client_ids.Add(clb_client, (int)cells[0]);
+                clb_client++;
+            }
+        }
+
+        // фнкция обработчик галочки Все в checkListBox1
+        private void ItemCheck(object sender, ItemCheckEventArgs e) {
+            CheckedListBox lb = sender as CheckedListBox;
+            if (e.Index == 0)
+            {
+                bool flag = e.NewValue == CheckState.Checked ? true : false;
+                for (int i = 1; i < lb.Items.Count; i++)
+                    lb.SetItemChecked(i, flag);
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // обработка кнопки 'Составить отчет'
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Дословно выполняет последнее предложение задания
+            // Не вдаваясь в подробности: По выбранным клиентам, группируем товары, вычисляем их кол-во и общую сумму. Разумеется для договоров с условиями на даты и статус
+            string sql = @"SELECT (SELECT go.title FROM Goods go WHERE cg.goods_id = go.goods_id), sum(cg.amount) as amount, sum(cg.price) as summary_price FROM (
+	                                SELECT cg1.goods_id, cg1.amount, cg1.price FROM Contract_goods cg1
+	                                JOIN Contract ct ON ct.contract_id = cg1.contract_id
+	                                WHERE ct.status = 'ready for shipment' and ct.client_id = ANY(:values) 
+			                                and ct.register_date >= :start_date and ct.register_date <= :end_date
+                                ) as cg
+                                GROUP BY cg.goods_id;";
+
+            List<int> values = new List<int>();
+            foreach (int index in checkedListBox1.CheckedIndices)
+                if (!(index is 0))
+                    values.Add(client_ids[index]);
+
+
+            NpgsqlCommand com = new NpgsqlCommand(sql, this.con);
+
+            com.Parameters.AddWithValue("values", values.ToArray());
+
+            NpgsqlParameter date1 = new NpgsqlParameter("start_date", NpgsqlTypes.NpgsqlDbType.Date);
+            date1.Value = dateTimePicker1.Value.Date;
+            com.Parameters.Add(date1);
+
+            NpgsqlParameter date2 = new NpgsqlParameter("end_date", NpgsqlTypes.NpgsqlDbType.Date);
+            date2.Value = dateTimePicker2.Value;
+            com.Parameters.Add(date2);
+
+            NpgsqlDataReader reader = com.ExecuteReader();
+            while (reader.Read()) {
+                string str = "";
+                string[] els = new string[report_columns.Length];
+                for (int i = 0; i < report_columns.Length; i++) {
+                    string el = reader[report_columns[i]].ToString();
+                    els[i] = el;
+                }
+                // складируем полученные данные
+                data.Add(els);
+            }
+            reader.Close();
+
+            make_report();
+        }
+
+        // функция для создния ексель-отчета 
+        private void make_report() {
+            // вот до 157 просто код из лаб
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            string filename = ofd.FileName;
+            Microsoft.Office.Interop.Excel.Application excelObject = new Microsoft.Office.Interop.Excel.Application();
+            excelObject.Visible = true;
+            Workbook wb = excelObject.Workbooks.Open(filename, 0, false, 5, "", "", false, XlPlatform.xlWindows, "", true, false, 0, true, false, false);
+            Worksheet wsh = wb.Sheets[1];
+            wsh.Columns.AutoFit();
+
+            // пишем в первую строчку названия колонок
+            for (int i = 0; i < ru_report_columns.Length; i++) {
+                wsh.Cells[1, i + 1] = ru_report_columns[i];
+            }
+
+            // потом построчно собранную инфу из data
+            for (int i = 0; i < data.Count; i++) {
+                for (int j = 0; j < report_columns.Length; j++) {
+                    wsh.Cells[i + 2, j + 1] = data[i][j];
+                }
+            }
+
+            // Дополнительно указываем период, за который был сдела отчет
+            wsh.Cells[1, ru_report_columns.Length + 1] = "Период";
+            wsh.Cells[2, ru_report_columns.Length + 1] = dateTimePicker1.Value.ToString();
+            wsh.Cells[3, ru_report_columns.Length + 1] = dateTimePicker2.Value.ToString();
+
+            wb.Save();
+            wb.Close();
+        }
+    }
+}
---
+++ RadProject/MainFrom.cs
@@ -0,0 +1,278 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RadProject
+{
+
+    public partial class MainFrom : Form
+    {
+
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        NpgsqlConnection con;
+
+        // DataGridView для отображение данных о товарах в конкретном договоре(подробнее в функции: dataGridView1_CellClick)
+        // помечу эту переменную как (*)
+        DataGridView goods;
+
+        // Словарь с русификацией для кнопок. В коде используются англ. значения для проверок, отображаются же по-русски
+        Dictionary<string, string[]> buttons_tables = new Dictionary<string, string[]>()
+        {
+            ["button1"] = new string[] { "Client", "Клиенты" },
+            ["button2"] = new string[] { "Goods", "Товары" },
+            ["button3"] = new string[] { "Contract", "Договоры" },
+            ["button4"] = new string[] { "Contract_Goods", "О договорах" }
+        };
+
+        // текущая таблица, можно поставить какую нибудь по-дефолту(см. названия кнопок)
+        string current_table = "";
+
+        public MainFrom()
+        {
+            InitializeComponent();
+            // Открытие соодиения с БД. Тут надо поменять пароль и название БД
+            this.con = new NpgsqlConnection(
+                    "Server=localhost; Port=5432; Username=postgres; Password=2305; database=RadStore"
+                );
+            con.Open();
+
+            // програмная инициализация (*)
+            /*
+                После выбора для отобржения таблицы Contract, под главным dataGridView1(собсна для отображения Contract)
+                будет создано отобнажение таблицы с данными о товарах в контракте(см. функцию dataGridView1_CellClick) - это goods (*)
+                и собсна для (*) задаем следующие параметры: Позицию, размер, задний фон, и т.д. 
+             */
+            goods = new DataGridView();
+            goods.Location = new Point(125, 350);
+            //contracts.Size = new Size(200, 200);
+            goods.Size = new Size(833, 250);
+            goods.MaximumSize = new Size(833, 250);
+            goods.BackgroundColor = System.Drawing.SystemColors.Control;
+            goods.Name = "goods";
+            goods.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            goods.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            goods.AutoSize = true;
+            goods.BorderStyle = BorderStyle.None;
+            goods.AllowUserToAddRows = false;
+
+        }
+
+        // функция для обновления содержания dataGridView1 - т.е. содержания соблиц из БД
+        private void update_view(string table)
+        {
+            // Очищает таблицу (*), чтобы не мешалась
+            if (this.Controls.Contains(goods))
+                goods.Columns.Clear();
+
+            // Опредляем в какую таблицу сейчас надо отобразить
+            if (current_table == "Client" || current_table == "Goods")
+            {
+                // Просто выводим всю инфу из таблицы
+                string sql = "SELECT * FROM " + current_table + ";";
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, this.con);
+                ds.Reset();
+                da.Fill(ds);
+                dt = ds.Tables[0];
+                dataGridView1.DataSource = dt;
+
+                // Отдельно меняем размер на указанный, потому что в случае таблицу Contract он будет изменен
+                // Если он и сейчас изменен из-за Contract, то меняем на исходный
+                dataGridView1.Size = new Size(833, 511);
+
+                // Руссификация названия колонок
+                if (current_table == "Client")
+                {
+                    // Задаем названия и инициализируем колонки
+                    string[] coColumns = { "Идентификатор", "Имя", "Фамилия" };
+                    foreach (DataGridViewColumn col in dataGridView1.Columns)
+                    {
+                        col.HeaderText = coColumns[col.Index];
+                    }
+                }
+                else {
+                    // Задаем названия и инициализируем колонки
+                    string[] coColumns = { "Идентификатор", "Название", "Описание", "Ед. измерения", "Цена"};
+                    foreach (DataGridViewColumn col in dataGridView1.Columns)
+                    {
+                        col.HeaderText = coColumns[col.Index];
+                    }
+                }
+            }
+            else if (current_table == "Contract") {
+                // Для таблицы Contract клиенты должны отображаться не в виде id, а 'по-человечески' - пускай будет отбражться фамилия
+                // Это собсна и делает следующий запрос
+                string sql = @"SELECT ct.contract_id, cl.last_name as client, ct.pay_type, ct.status, ct.register_date, ct.total_price 
+                                FROM Contract ct
+                                JOIN Client cl ON ct.client_id = cl.client_id;";
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, this.con);
+                ds.Reset();
+                da.Fill(ds);
+                dt = ds.Tables[0];
+                dataGridView1.DataSource = dt;
+
+                string[] coColumns = { "Идентификатор", "Клиент", "Тип оплаты", "Статус", "День регистрации", "Итоговая сумма" };
+                foreach (DataGridViewColumn col in dataGridView1.Columns) {
+                    col.HeaderText = coColumns[col.Index];
+                }
+
+                // Как и было объявлено раньше, для Contract размер задается другой, что бы не мешать отображению (*)
+                dataGridView1.Size = new Size(833, 250);
+            }
+            else if (current_table == "Contract_Goods")
+            {
+                // Для таблицы Contract товары должны отображаться не в виде id, а 'по-человечески' - пускай будет отбражться название товара
+                // Это собсна и делает следующий запрос
+                string sql = @"SELECT cg.contract_goods_id, cg.contract_id, go.title as goods, cg.amount, cg.price 
+                                FROM Contract_Goods cg
+                                JOIN Goods go ON go.goods_id = cg.goods_id;";
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, this.con);
+                ds.Reset();
+                da.Fill(ds);
+                dt = ds.Tables[0];
+                dataGridView1.DataSource = dt;
+                dataGridView1.Columns["contract_goods_id"].DisplayIndex = 0;
+                dataGridView1.Size = new Size(833, 511);
+
+                string[] coColumns = { "Идентификатор", "Договор", "Название товара", "Количество", "Итоговая сумма" };
+                foreach (DataGridViewColumn col in dataGridView1.Columns)
+                {
+                    col.HeaderText = coColumns[col.Index];
+                }
+            }
+
+            // сортируем таблицы по id 
+            dataGridView1.Sort(dataGridView1.Columns[current_table.ToLower() + "_id"], ListSortDirection.Ascending);
+        }
+
+
+        // Вспомогательная функцию для наполнения (*)
+        // Входные данные: id контракта(договора)
+        // Выходные данные: объект DataSource
+        private DataTable select_all_goods_from_contract(int id) {
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+
+            // Сложный запрос, в краце: среди товаров берет те, которые указаны в id-договоре, групирует по товарам, считает количество и сумму
+            string sql = @"SELECT go.title, go.description, go.unit, count(go.title), sum(go.price) FROM Goods go
+                            JOIN Contract_goods cg ON go.goods_id = cg.goods_id
+                            JOIN Contract ct ON ct.contract_id = cg.contract_id and ct.contract_id = " + id + " " +
+                            "GROUP BY cg.goods_id, go.title, go.description, go.unit, go.price;";
+
+            //MessageBox.Show(sql);
+
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter(sql, this.con);
+            ds.Reset();
+            da.Fill(ds);
+            dt = ds.Tables[0];
+            return dt;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Далее идет серия функций-обработчиков нажатия на копок
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // нажали на button1 - смотрим в словаре buttons_tables - её англ. значение таблицы
+            this.current_table = buttons_tables["button1"][0];
+            // а отображаем русское
+            table_label.Text = buttons_tables["button1"][1];
+            update_view(this.current_table);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.current_table = buttons_tables["button2"][0];
+            table_label.Text = buttons_tables["button2"][1];
+            update_view(this.current_table);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.current_table = buttons_tables["button3"][0];
+            table_label.Text = buttons_tables["button3"][1];
+            update_view(this.current_table);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.current_table = buttons_tables["button4"][0];
+            table_label.Text = buttons_tables["button4"][1];
+            update_view(this.current_table);
+        }
+
+        // Обработка меню-сверху: кнопка Составить отчет
+        private void отчетToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Запускаем новую форму
+            Form1 form = new Form1(this.con);
+            form.ShowDialog();
+        }
+
+        // Аналогично и с другими
+        private void добавитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddFrom form = new AddFrom(this.con, this.current_table);
+            form.ShowDialog();
+            update_view(current_table);
+        }
+
+        private void MainFrom_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void изменитьToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            int id = (int)dataGridView1.CurrentRow.Cells[current_table + "_id"].Value;
+            UpdateForm form = new UpdateForm(this.con, this.current_table, id);
+            form.ShowDialog();
+            update_view(current_table);
+        }
+
+        private void удалитьToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            // в данном случае берем id таблицы выбраной на dataGridView1 строке
+            int id = (int)dataGridView1.CurrentRow.Cells[current_table + "_id"].Value;
+            //  и удаляем из БД
+            NpgsqlCommand com = new NpgsqlCommand("DELETE FROM " + current_table + " WHERE " + current_table + "_id = " + id + ";", this.con);
+            com.Parameters.AddWithValue("id", id);
+            com.ExecuteNonQuery();
+            update_view(current_table);
+        }
+
+        // Функция наполения (*) - отображение данных о товарах в выбранном контракте
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // если текущая таблица не Contract - выходим
+            if (current_table != "Contract") return;
+
+            // Очищение (*) на случай, если мы переключились на другую запись Contract
+            if (this.Controls.Contains(goods))
+                goods.Columns.Clear();
+            
+            // наполнение (*)
+            goods.DataSource = select_all_goods_from_contract((int)dataGridView1.CurrentRow.Cells["Contract_id"].Value);
+
+            // русификация
+            string[] coColumns = { "Название товара", "Описание", "Ед. измерения", "Количество", "Сумма" };
+            foreach (DataGridViewColumn col in goods.Columns)
+            {
+                col.HeaderText = coColumns[col.Index];
+            }
+
+            // добавляем для отображени на полотне
+            this.Controls.Add(goods);
+        }
+    }
+}
